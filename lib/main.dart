605b44def26a794ac524945ba31e08d4c40cc3ca
FelipeJuarezJr <mr.felipe.juarez.jr@gmail.com>
2025-03-15 13:45:45 -0700

    Updated browser tab

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'screens/login_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
+  
+  try {
+    await dotenv.load();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    print('Firebase initialized successfully');
+  } catch (e) {
+    print('Error during initialization: $e');
+  }
+  
   runApp(const MyApp());
 }
 
@@ -23,56 +32,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: const InitializerWidget(),
-    );
-  }
-}
-
-class InitializerWidget extends StatefulWidget {
-  const InitializerWidget({super.key});
-
-  @override
-  _InitializerWidgetState createState() => _InitializerWidgetState();
-}
-
-class _InitializerWidgetState extends State<InitializerWidget> {
-  late Future<FirebaseApp> _initialization;
-
-  @override
-  void initState() {
-    super.initState();
-    _initialization = Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return const MaterialApp(
-            home: Scaffold(
-              body: Center(
-                child: Text('Error initializing Firebase'),
-              ),
-            ),
-          );
-        }
-
-        if (snapshot.connectionState == ConnectionState.done) {
-          return const LoginScreen();
-        }
-
-        return const MaterialApp(
-          home: Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          ),
-        );
-      },
+      home: const LoginScreen(),
     );
   }
 } 

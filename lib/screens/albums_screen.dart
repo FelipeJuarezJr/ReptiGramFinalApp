18aad991a4eba7c2d28952642029eaf30c09865e
FelipeJuarezJr <mr.felipe.juarez.jr@gmail.com>
2025-03-30 08:48:40 -0700

    Added Create Album & Add Image icons

--- lib/screens/albums_screen.dart
+++ lib/screens/albums_screen.dart
@@ -25,21 +25,38 @@ class _AlbumsScreenState extends State<AlbumsScreen> {
               const TitleHeader(),
               const Header(initialIndex: 1),
               Expanded(
-                child: SingleChildScrollView(
+                child: Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Column(
                     children: [
-                      // Album content will go here
-                      Container(
-                        padding: const EdgeInsets.all(16.0),
-                        child: const Text(
-                          'Albums Coming Soon',
-                          style: TextStyle(
-                            color: AppColors.titleText,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
+                      // Action Buttons at the top
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          _buildActionButton(
+                            'Create Album',
+                            Icons.create_new_folder,
+                            () {
+                              // TODO: Navigate to Create Album view
+                            },
                           ),
-                        ),
+                          _buildActionButton(
+                            'Add Image',
+                            Icons.add_photo_alternate,
+                            () {
+                              // TODO: Navigate to Add Image view
+                            },
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      // Albums Grid below
+                      _buildOptionCard(
+                        'Albums',
+                        Icons.photo_album,
+                        () {
+                          // TODO: Navigate to Albums view
+                        },
                       ),
                     ],
                   ),
@@ -47,6 +64,83 @@ class _AlbumsScreenState extends State<AlbumsScreen> {
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOptionCard(String title, IconData icon, VoidCallback onTap) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 200,  // Set a fixed height for the album card
+        decoration: BoxDecoration(
+          gradient: AppColors.inputGradient,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 5,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              size: 48,
+              color: AppColors.titleText,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              title,
+              style: const TextStyle(
+                color: AppColors.titleText,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildActionButton(String title, IconData icon, VoidCallback onTap) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+        decoration: BoxDecoration(
+          gradient: AppColors.loginGradient,
+          borderRadius: BorderRadius.circular(25),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 5,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Icon(
+              icon,
+              color: AppColors.buttonText,
+              size: 24,
+            ),
+            const SizedBox(width: 8),
+            Text(
+              title,
+              style: const TextStyle(
+                color: AppColors.buttonText,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
       ),
     );

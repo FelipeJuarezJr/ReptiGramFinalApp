3cb5c5ca634a5f4a93a50e6a90b1c04691c47125
FelipeJuarezJr <mr.felipe.juarez.jr@gmail.com>
2025-03-30 09:24:41 -0700

    Added Binders within Albums

--- lib/screens/binders_screen.dart
+++ lib/screens/binders_screen.dart
@@ -2,17 +2,21 @@ import 'package:flutter/material.dart';
 import '../styles/colors.dart';
 import '../common/header.dart';
 import '../common/title_header.dart';
-import '../screens/binders_screen.dart';
 
-class AlbumsScreen extends StatefulWidget {
-  const AlbumsScreen({super.key});
+class BindersScreen extends StatefulWidget {
+  final String albumName;
+
+  const BindersScreen({
+    super.key,
+    required this.albumName,
+  });
 
   @override
-  State<AlbumsScreen> createState() => _AlbumsScreenState();
+  State<BindersScreen> createState() => _BindersScreenState();
 }
 
-class _AlbumsScreenState extends State<AlbumsScreen> {
-  List<String> albums = ['My Album']; // Initial album, add more as created
+class _BindersScreenState extends State<BindersScreen> {
+  List<String> binders = ['Binder 1']; // Initial binder
 
   @override
   Widget build(BuildContext context) {
@@ -37,10 +41,10 @@ class _AlbumsScreenState extends State<AlbumsScreen> {
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
                           _buildActionButton(
-                            'Create Album',
+                            'Create Binder',
                             Icons.create_new_folder,
                             () {
-                              _createNewAlbum();
+                              _createNewBinder();
                             },
                           ),
                           _buildActionButton(
@@ -53,7 +57,16 @@ class _AlbumsScreenState extends State<AlbumsScreen> {
                         ],
                       ),
                       const SizedBox(height: 20),
-                      // Albums Grid below
+                      Text(
+                        widget.albumName,
+                        style: const TextStyle(
+                          color: AppColors.titleText,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      // Binders Grid below
                       Expanded(
                         child: GridView.builder(
                           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -61,9 +74,9 @@ class _AlbumsScreenState extends State<AlbumsScreen> {
                             crossAxisSpacing: 16,
                             mainAxisSpacing: 16,
                           ),
-                          itemCount: albums.length,
+                          itemCount: binders.length,
                           itemBuilder: (context, index) {
-                            return _buildAlbumCard(albums[index]);
+                            return _buildBinderCard(binders[index]);
                           },
                         ),
                       ),
@@ -78,26 +91,25 @@ class _AlbumsScreenState extends State<AlbumsScreen> {
     );
   }
 
-  void _createNewAlbum() {
-    // Show dialog to enter album name
+  void _createNewBinder() {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        String newAlbumName = '';
+        String newBinderName = '';
         return AlertDialog(
           backgroundColor: AppColors.dialogBackground,
           title: const Text(
-            'Create New Album',
+            'Create New Binder',
             style: TextStyle(color: AppColors.titleText),
           ),
           content: TextField(
             style: const TextStyle(color: AppColors.titleText),
             decoration: const InputDecoration(
-              hintText: 'Enter album name',
+              hintText: 'Enter binder name',
               hintStyle: TextStyle(color: Colors.grey),
             ),
             onChanged: (value) {
-              newAlbumName = value;
+              newBinderName = value;
             },
           ),
           actions: [
@@ -110,9 +122,9 @@ class _AlbumsScreenState extends State<AlbumsScreen> {
             TextButton(
               child: const Text('Create'),
               onPressed: () {
-                if (newAlbumName.isNotEmpty) {
+                if (newBinderName.isNotEmpty) {
                   setState(() {
-                    albums.add(newAlbumName);
+                    binders.add(newBinderName);
                   });
                   Navigator.of(context).pop();
                 }
@@ -121,53 +133,6 @@ class _AlbumsScreenState extends State<AlbumsScreen> {
           ],
         );
       },
-    );
-  }
-
-  Widget _buildAlbumCard(String albumName) {
-    return InkWell(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => BindersScreen(albumName: albumName),
-          ),
-        );
-      },
-      child: Container(
-        decoration: BoxDecoration(
-          gradient: AppColors.inputGradient,
-          borderRadius: BorderRadius.circular(15),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.2),
-              blurRadius: 5,
-              offset: const Offset(0, 3),
-            ),
-          ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(
-              Icons.folder,
-              size: 48,
-              color: AppColors.titleText,
-            ),
-            const SizedBox(height: 8),
-            Text(
-              albumName,
-              style: const TextStyle(
-                color: AppColors.titleText,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ],
-        ),
-      ),
     );
   }
 
@@ -205,6 +170,43 @@ class _AlbumsScreenState extends State<AlbumsScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildBinderCard(String binderName) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: AppColors.inputGradient,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(
+            Icons.folder_special,
+            size: 48,
+            color: AppColors.titleText,
+          ),
+          const SizedBox(height: 8),
+          Text(
+            binderName,
+            style: const TextStyle(
+              color: AppColors.titleText,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ],
       ),
     );
   }
